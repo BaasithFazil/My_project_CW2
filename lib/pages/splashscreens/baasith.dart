6fc0e8a3b2f0f4fdb2e4c06341f69eceafe3ae8f
BaasithFazil <fazilbaasith@gmail.com>
2022-11-18 22:04:01 +0530

    Added Success

---
+++ lib/pages/splashscreens/baasith.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class Baasith extends StatelessWidget {
+  const Baasith({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
